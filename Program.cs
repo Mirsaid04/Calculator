285fc291489e07e9aa1ad1302552952e85c08887
Mirsaid04 <mirsaidd04@gmail.com>
2023-07-20 21:51:12 +0900

    Upgrade project

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@
             Console.Write("Second number: ");
             string secondUservalue=Console.ReadLine()!;
 
-            Console.WriteLine("Converting value in progress...");
+            Console.WriteLine("\nConverting value in progress...\n");
             decimal firstNumber=Convert.ToDecimal(firstUservalue);
             decimal secondNumber=Convert.ToDecimal(secondUservalue);
 
@@ -20,7 +20,34 @@
             Console.WriteLine($"{firstNumber}-{secondNumber}={firstNumber-secondNumber}");
             Console.WriteLine($"{firstNumber}*{secondNumber}={firstNumber*secondNumber}");
             Console.WriteLine($"{firstNumber}/{secondNumber}={firstNumber/secondNumber}");
-            Console.WriteLine($"{firstNumber}%{secondNumber}={firstNumber%secondNumber}");
+            Console.WriteLine($"{firstNumber}%{secondNumber}={firstNumber%secondNumber}\n");
+
+            Console.WriteLine("New project is starting....\n");
+
+            System.Console.Write("Do you want to go to the army,please enter your age\nAge: ");
+            string userAgeValue=Console.ReadLine()!;
+            int ageValue=int.Parse(userAgeValue);
+
+            if (ageValue>=2218 && ageValue < 28)
+            {
+                System.Console.WriteLine("Your age is eligible to go");
+            }
+            else
+            System.Console.WriteLine("Your age is not eligible to go\n");
+
+            System.Console.WriteLine("This is also same project but in other way\n");
+
+            System.Console.WriteLine("It is starting now...\n");
+
+            System.Console.Write("Enter your age: ");
+            string secondAgevalue=Console.ReadLine()!;
+            int secondAge=Convert.ToInt32(secondAgevalue);
+
+            string message=(secondAge >= 18 && secondAge < 28)
+                                ? "You can go to the army"
+                                : "You can not go to the army";
+            System.Console.WriteLine(message);
+            System.Console.WriteLine("\n<--Thank you for yout attenton-->");
         }
     }
 }
