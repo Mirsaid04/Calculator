a9167cc8d4a73bdd477d0df48bd3a9e282bda01e
Mirsaid04 <mirsaidd04@gmail.com>
2023-07-20 20:32:53 +0900

    New project file

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            // type name =Value;
+            Console.WriteLine("Enter values: ");
+            Console.Write("First  number: ");
+            string firstUservalue=Console.ReadLine()!;
+            Console.Write("Second number: ");
+            string secondUservalue=Console.ReadLine()!;
+
+            Console.WriteLine("Converting value in progress...");
+            decimal firstNumber=Convert.ToDecimal(firstUservalue);
+            decimal secondNumber=Convert.ToDecimal(secondUservalue);
+
+            Console.WriteLine("Here are your results: ");
+            Console.WriteLine($"{firstNumber}+{secondNumber}={firstNumber+secondNumber}");
+            Console.WriteLine($"{firstNumber}-{secondNumber}={firstNumber-secondNumber}");
+            Console.WriteLine($"{firstNumber}*{secondNumber}={firstNumber*secondNumber}");
+            Console.WriteLine($"{firstNumber}/{secondNumber}={firstNumber/secondNumber}");
+            Console.WriteLine($"{firstNumber}%{secondNumber}={firstNumber%secondNumber}");
+        }
+    }
+}
