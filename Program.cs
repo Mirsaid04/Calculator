3684473542ef8a188039d23e6e9bee0b8def85ee
Mirsaid04 <mirsaidd04@gmail.com>
2023-07-28 19:00:03 +0900

    C#-Project-Catch-FormatException

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main()
         {
+            try{
             string yesOrno="";
             do{
             Console.WriteLine("Welcome to  Calculator Application");
@@ -59,6 +60,12 @@
                 {
                     Console.WriteLine($"{outeriteration} * {iteration} = {outeriteration*iteration} ");
                 }
+            }
+            }
+            catch(FormatException FormatException)
+            {
+                Console.WriteLine("Oops,We could not convert your input value.");
+                System.Console.WriteLine("Looks like the value you provided is not integer");
             }
 
         }
