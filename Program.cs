745dade8a1d74858d325597e4fb4a22772c309cb
Mirsaid04 <mirsaidd04@gmail.com>
2023-08-17 17:25:13 +0900

    Update-With-Inheritance

--- Program.cs
+++ Program.cs
@@ -1,26 +1,49 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using Calculator.Classes;
 
 namespace Calculator
 {
     public class Program
     {
-        static void Main()
+         static void Main()
         {
-            var calculator = new CalculatR();
-
             string yesOrno = "";
             do
             {
-                calculator.Calculate();
-                ReportR.ShowGratitude();
+                ValueManipulatR.ShowMenu();
+            int choiceOfOption = ReportR.choseedNumberConverted();
+            switch (choiceOfOption)
+            {
+                case 1 :
+                    decimal firstNumber = ValueManipulatR.GetUserValueByMessage("Welcome to  Calculator Application,\nEnter first number: ");
+                    string option=ValueManipulatR.GetInputByMessageToAction("Choose operation that you need [+ , - , * , / , % , percent]: ");
+                    if (option == "percent")
+                    {
+                        decimal secondNumber = ValueManipulatR.GetUserValueByMessage("\nPlease enter the Percent: ");
+                        var calculator = new AdvancedCalculator(firstNumber,option,secondNumber);
+                        break;
+
+                    }
+                    else
+                    {
+                        decimal secondNumber = ValueManipulatR.GetUserValueByMessage("\nSecond number: ");
+                        var calculatR = new CalculatR(firstNumber, option, secondNumber);
+                        break;
+
+                    }
+                case 2 :MultiplicationTableR.ShowMultiplicationTable();
+                    ReportR.ShowGratitude();
+                    break;
+            }
                 
                 Console.WriteLine("Do you want to continue : [y/n]");
                 yesOrno = Console.ReadLine()!;
             } while (yesOrno == "y");
 
             ReportR.ShowGratitude();
-            calculator.ShowMultiplicationTable();
+            
         }
     }
 }
