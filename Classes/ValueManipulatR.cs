2bae3ccfdf739b09a54d4ff25dba04a779306b2d
Mirsaid04 <mirsaidd04@gmail.com>
2023-08-17 17:25:54 +0900

    Gets-Input-By-User

--- Classes/ValueManipulatR.cs
+++ Classes/ValueManipulatR.cs
@@ -1,10 +1,24 @@
 namespace Calculator.Classes;
 
-    public static class ValueManipulatR
+public static class ValueManipulatR
     {
-        public static string GetUserValueByMessage(string message)
+        public static decimal GetUserValueByMessage(string message)
         {
-        Console.Write(message);
-        return Console.ReadLine()!;
+            Console.Write(message);
+            return decimal.Parse(Console.ReadLine());
+        }
+        public static string GetInputByMessageToAction(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+        
+        public static void ShowMenu()
+        {
+            Console.Write("\nWelcome.\n1.Calculator" +
+                          "\n2.Multiplicator" +
+                          "\n3.Exit" +
+                          "\nChoice: ");
         }
     }
+    
